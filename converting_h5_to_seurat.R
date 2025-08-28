d156b59b5269dfead05b4c35ec6d141c439da6ba
DanM <d.mccluskey@ucl.ac.uk>
2025-08-28 12:05:09 +0100

    update

--- converting_h5_to_seurat.R
+++ converting_h5_to_seurat.R
@@ -1,6 +1,7 @@
 #converting h5 (normally anndata from scanpy workflow) files to seurat objects
+setwd("/Users/dan94/OneDrive - University College London/UCL_Senior_Research_Fellow/RIPs_Vincent_project/")
 library(zellkonverter)
 library(Seurat)
-poms <- readH5AD("poms.annotated.dan.h5ad")
-poms.seurat <- as.Seurat(poms)
-rm(poms)
+data <- readH5AD("GSE174188_CLUES1_adjusted.h5ad")
+data.seurat <- as.Seurat(data)
+rm(data)
